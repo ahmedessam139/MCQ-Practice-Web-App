c86900f3cfb61b7f5dc862e5f76c0ee34f990ebf
ahmedessam139 <104240439+ahmedessam139@users.noreply.github.com>
2023-01-17 21:53:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+  var currentQuestionIndex = 0;
+  var questionsData;
+  var wrongCounter = 0;
+  var totalQuestions = 0;
+  var displayedQuestions = [];
+  var randomIndex = 0;
+  var questionNum = 0;
+  var jsonFile = "fr1.json"; 
+  $.getJSON(jsonFile, function (data) {
+      questionsData = data;
+      totalQuestions = questionsData.length;
+      displayQuestion();
+  });
+  $("#qs-button").click(function () {
+      jsonFile = "fr1.json";
+      // Reload the JSON data with new path
+      $.getJSON(jsonFile, function (data) {
+          questionsData = data;
+          totalQuestions = questionsData.length;
+          displayedQuestions = [];
+          randomIndex = 0;
+          questionNum = 0;
+          wrongCounter = 0;
+          displayQuestion();
+      });
+  });
+  $("#G-button").click(function () {
+      jsonFile = "5G.json";
+      $.getJSON(jsonFile, function (data) {
+          questionsData = data;
+          totalQuestions = questionsData.length;
+          displayedQuestions = [];
+          randomIndex = 0;
+          questionNum = 0;
+          wrongCounter = 0;
+          displayQuestion();
+      });
+  });
+  function displayQuestion() {
+    randomIndex = Math.floor(Math.random() * totalQuestions);
+    while (displayedQuestions.indexOf(randomIndex) !== -1) {
+      randomIndex = Math.floor(Math.random() * totalQuestions);
+    }
+    displayedQuestions.push(randomIndex);
+    $("#question").html(questionsData[randomIndex].Questions);
+    $("#labelA").html(questionsData[randomIndex].A);
+    $("#labelB").html(questionsData[randomIndex].B);
+    $("#labelC").html(questionsData[randomIndex].C);
+    $("#labelD").html(questionsData[randomIndex].D);
+    $(".answer input").prop("checked", false);
+    $("#result").html("");
+    $("#counter1").html("Question:" + (questionNum + 1));
+    $("#counter").html("Your score is: " + (questionNum - wrongCounter) + " out of " + totalQuestions);
+
+  }
+
+  $("#submit-button").click(function () {
+    var selectedAnswer = $(".answer input:checked");
+    if (selectedAnswer.length) {
+      var selectedAnswerValue = $(".answer input:checked + label").text();
+      if (selectedAnswerValue.charAt(0) === questionsData[randomIndex].Answer) {
+        $("#result").html("Correct!");
+        $("#result").css("color", "green");
+        questionNum += 1
+        if (displayedQuestions.length === totalQuestions) {
+          setTimeout(() => { document.location.reload(); }, 4000);
+          $("#counter").html("Your score is: " + (totalQuestions - wrongCounter) + " out of " + totalQuestions);
+          alert("Your score is: " + (totalQuestions - wrongCounter) + " out of " + totalQuestions);
+        }
+        else {
+          setTimeout(() => { currentQuestionIndex++; displayQuestion(); }, 1000);
+        }
+      } else {
+        $("#result").html("Incorrect. The correct answer is " + questionsData[randomIndex].Answer);
+        $("#result").css("color", "red");
+        wrongCounter++;
+      }
+    } else {
+      $("#result").html("Please select an answer");
+      $("#result").css("color", "blue");
+    }
+  });
+
+  $(".answer input").change(function () {
+    if (this.checked) {
+      $(".answer input").not(this).prop("checked", false)
+    }
+  });
+
+});
+
+
