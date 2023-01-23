e10405475ecc69e71541ce1558c8f7cea47605e1
Ahmed_Essam <ahmedessam139ae@gmail.com>
2023-01-23 14:50:48 +0200

    modified:   index.html 	modified:   qs.json 	modified:   script.js 	new file:   sheet(T-F).json 	new file:   sheet.json 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 $(document).ready(function () {
-  var currentQuestionIndex = 0;
+  var jsonFile = "qs.json";
   var questionsData;
   var wrongCounter = 0;
   var totalQuestions = 0;
   var displayedQuestions = [];
-  var randomIndex = 0;
   var questionNum = 0;
-  var jsonFile = "qs.json";
+  var randomIndex = 0;
+  var WrongQustionsIndex = [];
+
   $.getJSON(jsonFile, function (data) {
     questionsData = data;
     totalQuestions = questionsData.length;
     displayQuestion();
   });
+
+  $('.dropdown-select').on('change', function(){
+    jsonFile = $(this).val();
+    resetQuiz();
+    $(this)[0].selectedIndex = 0;
+  });
+
   function displayQuestion() {
     randomIndex = Math.floor(Math.random() * totalQuestions);
     while (displayedQuestions.indexOf(randomIndex) !== -1) {
@@ -27,7 +35,9 @@ $(document).ready(function () {
     $("#result").html("");
     $("#counter1").html("Question:" + (questionNum + 1));
     $("#counter").html("Your score is: " + (questionNum - wrongCounter) + " out of " + totalQuestions);
+
   }
+
   $("#submit-button").click(function () {
     var selectedAnswer = $(".answer input:checked");
     if (selectedAnswer.length) {
@@ -35,30 +45,86 @@ $(document).ready(function () {
       if (selectedAnswerValue.charAt(0) === questionsData[randomIndex].Answer) {
         $("#result").html("Correct!");
         $("#result").css("color", "green");
-        questionNum += 1
+        questionNum += 1;
         if (displayedQuestions.length === totalQuestions) {
-          setTimeout(() => { document.location.reload(); }, 4000);
+          setTimeout(() => { displayWrongQuestions(); }, 2500);
           $("#counter").html("Your score is: " + (totalQuestions - wrongCounter) + " out of " + totalQuestions);
           alert("Your score is: " + (totalQuestions - wrongCounter) + " out of " + totalQuestions);
         }
         else {
-          setTimeout(() => { currentQuestionIndex++; displayQuestion(); }, 1000);
+          setTimeout(() => { displayQuestion(); }, 250);
         }
       } else {
         $("#result").html("Incorrect. The correct answer is " + questionsData[randomIndex].Answer);
         $("#result").css("color", "red");
         wrongCounter++;
+        WrongQustionsIndex.push(randomIndex);
+
       }
     } else {
       $("#result").html("Please select an answer");
       $("#result").css("color", "blue");
     }
   });
+
   $(".answer input").change(function () {
-    if (this.checked) {
-      $(".answer input").not(this).prop("checked", false)
+    if (this.checked ) {
+      $(".answer input").not(this).prop("checked", false);
     }
   });
+  function resetQuiz() {
+    $.getJSON(jsonFile, function (data) {
+      questionsData = data;
+      totalQuestions = questionsData.length;
+      displayedQuestions = [];
+      questionNum = 0;
+      wrongCounter = 0;
+      localStorage.removeItem("quizData");
+      displayQuestion();
+    });
+  }
+  function displayWrongQuestions() {
+    $("body").empty();
+    let label = $('<div>',{class:"counter-label",id:'counter'}).text("Wrong Questions")
+    label.css({
+      "position":"relative",
+      "top":"auto",
+      "left":"auto",
+      "transform":"translate(0, 0)"
+    })
+    $('body').append(label);
+    for (let i = 0; i < WrongQustionsIndex.length; i++) {
+      let question = questionsData[WrongQustionsIndex[i]];
+      let questionContainer = $('<div>', { class: 'question-container', style: "border: 1px solid red;"});
+      let questionDiv = $('<div>', { id: 'question' }).text(question.Questions);
+      let answersContainer = $('<div>', { class: 'answers-container' });
+    
+      let answers = ["A", "B", "C", "D"];
+      for (let j = 0; j < answers.length; j++) {
+        let answer = answers[j];
+        let answerDiv = $('<div>', { class: 'answer' });
+        let label = $('<label>', { id: 'label' + answer }).text(question[answer]);
+        if (question.Answer === answer) {
+          label.css("color", "red");
+        }
+        answerDiv.append(label);
+        answersContainer.append(answerDiv);
+      }
+    
+      questionContainer.append(questionDiv);
+      questionContainer.append(answersContainer);
+      
+      setTimeout(function() {
+        questionContainer.hide();
+        questionContainer.fadeIn(1500);
+        $('body').append(questionContainer);
+        $("body").css("height", "auto");
+      }, i * 500);
+    }
+    
+  }
+  $("#end-button").click(displayWrongQuestions)
+
 });
 
 
